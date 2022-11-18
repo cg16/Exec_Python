557e214cc106ff9fdbfc02b1e5d33448abaade0c
Celso Guilherme <78118860+cg16@users.noreply.github.com>
2022-11-17 23:47:20 -0300

    Delete 1_6 directory

--- 1_6/Exec001.py
+++
@@ -1,4 +0,0 @@
-#Recebe nome e mostra uma mensagem de bem vindo#
-nome = str(input('Qual o seu nome?'))
-
-print('Olá {} seja bem vindo!'.format(nome))
--- 1_6/Exec004.py
+++
@@ -1,5 +0,0 @@
-#Ler o dado informado do usuario e informando seu tipo#
-datauser = input("Digite algo")
-print(type(datauser))
-
-
