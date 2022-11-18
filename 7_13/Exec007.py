eb540c1f00a977170475b73a10a7b03073f9eb69
Celso Guilherme <78118860+cg16@users.noreply.github.com>
2022-11-17 23:51:08 -0300

    Delete 7_13 directory

--- 7_13/Exec007.py
+++
@@ -1,6 +0,0 @@
-#Ler duas notas inseridas pelo usuario e calcula sua media#
-
-n1 = float(input('Insira sua primeira nota: '))
-n2 = float(input('Insira a segunda nota: '))
-media = (n1 + n2) / 2
-print('A sua media é {:.2f}'.format(media))
